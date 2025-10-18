315d7043f21dc21eae11750eee9b96dab9b4ffc9
sahil7751 <sahiljirapure8@gmail.com>
2025-10-18 14:19:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# coding-project-template
+
+Demo link: https://www.youtube.com/watch?si=KL1kZWutEbGLBWKC&v=fw3oM-svVJU&feature=youtu.be
---
+++ giftlink-frontend/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import Profile from './components/Profile/Profile';
+import DetailsPage from './components/DetailsPage/DetailsPage';
+import SearchPage from './components/SearchPage/SearchPage';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+        <>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
+          <Route path="/app/profile" element={<Profile />} />
+          <Route path="/app/product/:productId" element={<DetailsPage/>} />
+          <Route path="/app/search" element={<SearchPage/>} />
+        </Routes>
+        </>
+  );
+}
+export default App;
